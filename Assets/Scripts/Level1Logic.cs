e690058f58b4aedc032de063f0ad77887a9c069d
Rodrigo Geve <rodgeve@gmail.com>
2018-05-03 07:28:07 -0400

    Latest changes / Kill Player functionality

--- Assets/Scripts/Level1Logic.cs
+++ Assets/Scripts/Level1Logic.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Runtime.InteropServices;
 
 public class Level1Logic : MonoBehaviour {
+    
+    public bool ProductionMode { get { return production_mode; } }
 
     public GameObject Vehicle { get { return prefab; } }
     public GameObject Vehicle2 { get { return prefab2; } }
@@ -12,9 +15,16 @@ public class Level1Logic : MonoBehaviour {
     public GameObject BillboardText { get { return billboardText; } }
     public GameObject IntroCarGroup { get { return introCarGroup; } }
 
+    public GameObject SpawnPoints { get { return spawnPointsGroup; } }
+    
     public Camera Camera1 { get { return camera1; } }
     public Camera Camera2 { get { return camera2; } }
     public Camera BillboardCamera { get { return camera3; } }
+
+    public GameObject StartGameButtonText { get { return startGameButtonText; } }
+
+
+    [SerializeField] private bool production_mode;
 
     [SerializeField] private GameObject prefab;
     [SerializeField] private GameObject prefab2;
@@ -24,12 +34,26 @@ public class Level1Logic : MonoBehaviour {
 
     [SerializeField] private GameObject introCarGroup;
 
+    [SerializeField] private GameObject spawnPointsGroup;
+
 
     [SerializeField] private Camera camera1;
     [SerializeField] private Camera camera2;
     [SerializeField] private Camera camera3;
 
+    [SerializeField]  private GameObject startGameButtonText;
 
+
+    [DllImport("__Internal")]
+    private static extern void ConfigureGameOptions(int maxPlayers);
+    [DllImport("__Internal")]
+    private static extern void NewGameLobby();
+    [DllImport("__Internal")]
+    private static extern void GameStart(int numStartingPlayers);
+    [DllImport("__Internal")]
+    private static extern void PlayerSubstitution(int playerId, string gamerTag, string colour);
+    [DllImport("__Internal")]
+    private static extern void KillPlayer(int playerId, string gamerTag);
 
     private const int MAX_VEHICLES = 8;
 
@@ -39,23 +63,33 @@ public class Level1Logic : MonoBehaviour {
     private GameObject[] vehicles = new GameObject[MAX_VEHICLES];
     private string[] playerTags = new string[MAX_VEHICLES];
     private int[] playerScores = new int[MAX_VEHICLES];
-    
+
+    private string[] carColours = { "Purple", "Dark Orange", "Blue", "Light Orange", "Yellow", "Green", "Grey", "White", "Pink", "Cyan" };
+
     private PlayerDamage[] playerHealth = new PlayerDamage[MAX_VEHICLES];
 
-    private TextMeshPro mText;
+    private TextMeshPro mText = null;
+    private TextMeshProUGUI startButtonText = null;
 
-    private Vector3 spawnPoint1 = new Vector3(-111.62f, 15f, -75.16f);
-    private Vector3 spawnPoint2 = new Vector3(48.64f, 15f, -51.8f);
+
+    private Vector3[] spawnPoints = new Vector3[MAX_VEHICLES];
     
     private int currentGameCameraIndex = 0;
 
-    // Init
+    private int numStartingPlayers = 0;
+
     void Awake()
     {
         mText = billboardText.GetComponent<TextMeshPro>();
+        startButtonText = startGameButtonText.GetComponent<TextMeshProUGUI>();
+        startButtonText.text = "Start";
 
-        StartGameLobby();
-      
+        for (int i=0; i < MAX_VEHICLES; i++)
+        {
+            spawnPoints[i] = spawnPointsGroup.transform.GetChild(i).transform.position;
+        }
+
+        NewGame();
     }
 
     public void DisplayBillboardText(string text)
@@ -83,6 +117,9 @@ public class Level1Logic : MonoBehaviour {
 
                 camera3.enabled = false;
                 camera3.tag = "Untagged";
+
+                camera1.GetComponent<Animator>().Play("Camera1", -1, 0f);
+
                 break;
             case 1:
                 camera1.enabled = false;
@@ -93,6 +130,7 @@ public class Level1Logic : MonoBehaviour {
 
                 camera3.enabled = true;
                 camera3.tag = "Untagged";
+                
                 break;
             case 2:
             default:
@@ -104,10 +142,24 @@ public class Level1Logic : MonoBehaviour {
 
                 camera3.enabled = true;
                 camera3.tag = "MainCamera";
+
+                camera3.GetComponent<Animator>().Play("BillBoardCam", -1, 0f);
                 break;
         }
     }
-    public void PlayerSubstitution (int playerId, string playerTag, int score, int lives)
+
+    public void SubstitutePlayer(string encodedParams)
+    {
+        string[] args = encodedParams.Split(',');
+
+        int playerId = int.Parse(args[0]);
+        string playerTag = args[1];
+        int score = int.Parse(args[2]);
+
+        PlayerSubstitution(playerId, playerTag, score);
+    }
+        
+    private void PlayerSubstitution (int playerId, string playerTag, int score)
     {
         int i = playerId;
         if ((gameState == States.GameLobby || gameState == States.GameStarted) &&
@@ -116,55 +168,50 @@ public class Level1Logic : MonoBehaviour {
             // Initialize player vehicle
             //
             GameObject spawnVehicle = prefab;
-         
-            //if (currentSpawnPoint == 1)
-            //{
-            //    spawnVehicle = prefab2;
-            //}
-
+        
             playerTags[i] = playerTag;
             playerScores[i] = score;
-            
+
             // Create player vehicle
+            if (vehicles[i]) Destroy(vehicles[i]);
             vehicles[i] = Instantiate(spawnVehicle, containerGroup.transform);
             vehicles[i].name = "Vehicle-" + i.ToString();
 
             playerHealth[i] = vehicles[i].GetComponent<PlayerDamage>();
 
-            respawnPlayer(i);
+            respawnPlayerAtPosition(i);
 
             MeshRenderer mr = vehicles[i].transform.GetChild(2).GetComponent<MeshRenderer>();
             Material[] materials = new Material[1];
             string resName = "MonsterTruck/Materials/car_03_m" + (i + 1).ToString();
             materials[0] = (Material)Resources.Load(resName, typeof(Material));
             mr.materials = materials;
+
+            // Count number of players added so far
+            //
+            numStartingPlayers++;
+            if (numStartingPlayers > MAX_VEHICLES) numStartingPlayers = MAX_VEHICLES ;
+
+            // Notify HTML Page of Player Substitution Event
+            //
+            if (production_mode) PlayerSubstitution(i, playerTags[i], carColours[i]);
         }
     }
 
-    private void respawnPlayer(int i)
+    private void respawnPlayerAtPosition(int i)
     {
         int currentSpawnPoint = i & 1;
 
-        Vector3 spawnPosition = spawnPoint1;
-        if (currentSpawnPoint == 1)
-        {
-            spawnPosition = spawnPoint2;
-        }
-
+        // Position to Spawn point and rotate to hill target
+        Vector3 spawnPosition = spawnPoints[i];
         vehicles[i].transform.SetPositionAndRotation(spawnPosition, Quaternion.identity);
-
-        if (currentSpawnPoint == 0)
-        {
-            vehicles[i].transform.Rotate(Vector3.up * 90f);
-        }
-        else
-        {
-            vehicles[i].transform.Rotate(Vector3.up * -90f);
-        }
+        vehicles[i].transform.LookAt(spawnPoints[1], Vector3.up);
     }
 
     public void StartGameLobby ()
     {
+        if (production_mode) ConfigureGameOptions(MAX_VEHICLES);
+
         // TODO - Reset to Game Lobby State
         gameState = States.GameLobby;
 
@@ -178,26 +225,60 @@ public class Level1Logic : MonoBehaviour {
             if (vehicles[i]) Destroy(vehicles[i]);
         }
 
-        // Wait for Players
-    }
-    public void StartGame ()
-    {
-        // TODO - Reset to Game Start state
-        gameState = States.GameStarted;
-
-        switchCameraView(0);
-
-        // Remove Game Intro Objects
+        // Notify HTML Page of Player Substitution Event
         //
-        introCarGroup.SetActive(false);
-        for (int i = 0; i < vehicles.Length; i++)
-        {
-            if (vehicles[i]) Destroy(vehicles[i]);
-        }
+        if (production_mode) NewGameLobby();
 
-        // Create new Vehicles
-        PlayerSubstitution(0, "Player1", 0, 1);
-        PlayerSubstitution(1, "Player2", 0, 1);
+        // Wait for Players
+
+    }
+    public void StartGame()
+    {
+        if (gameState == States.GameLobby)
+        {
+            // Game State is now 'Game Started'
+            //
+            gameState = States.GameStarted;
+
+            switchCameraView(0);
+
+            // Remove Game Intro Objects
+            //
+            introCarGroup.SetActive(false);
+            for (int i = 0; i < vehicles.Length; i++)
+            {
+                if (vehicles[i]) Destroy(vehicles[i]);
+            }
+
+            // Notify HTML Page of Game Started event
+            //
+            if (production_mode) GameStart(numStartingPlayers);
+
+            // Now that the game is started, The 'Start' button will
+            //   take on the role of cycling players through the game (i.e. Kill off a player)
+            startButtonText.text = "Next Plyr";
+
+            // Stop Lobby Music
+            //
+            AudioSource aSrc = transform.gameObject.GetComponent<AudioSource>();
+            aSrc.Stop();
+            
+        }
+        else if (gameState == States.GameStarted)
+        {
+            // Game is Started / Kill Player Mode
+            //
+            for (int i = 0; i < vehicles.Length; i++)
+            {
+                if (vehicles[i] != null)
+                {
+                    // Kill off one player
+                    //
+                    killPlayer(i);
+                    break;
+                }
+            }
+        }
     }
 
     // Use this for initialization
@@ -217,18 +298,35 @@ public class Level1Logic : MonoBehaviour {
             {
                 if (playerHealth[i].PlayerHealth < 0)
                 {
-                    // Respawn
-                    vehicles[i].GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
-                    vehicles[i].GetComponent<Rigidbody>().angularVelocity = new Vector3(0f, 0f, 0f);
-
-
-                    playerHealth[i].PlayerHealth = 9000;
-                    playerHealth[i].InflictDamage(0);
-                    respawnPlayer(i);
+                    killPlayer(i);
                 }
             }
         }
         
+    }
+
+    private void killPlayer(int playerId)
+    {
+        // Player health below 0 / Player Killed
+        //
+        if (vehicles[playerId] != null)
+        {
+            vehicles[playerId].GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
+            vehicles[playerId].GetComponent<Rigidbody>().angularVelocity = new Vector3(0f, 0f, 0f);
+
+
+            playerHealth[playerId].PlayerHealth = 9000;
+            playerHealth[playerId].InflictDamage(0);
+
+            Destroy(vehicles[playerId]);
+            vehicles[playerId] = null;
+
+            //respawnPlayerAtPosition(playerId);
+
+            // Notify HTML Page of Player Kill event
+            //
+            if (production_mode) KillPlayer(playerId, playerTags[playerId]);
+        }
     }
 
     private States GameState
